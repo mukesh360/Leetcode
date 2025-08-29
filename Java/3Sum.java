c542b9b9e7f3c0ad89ed364a1ec1da3ddedc3f30
mukesh360 <mugimugesh023@gmail.com>
2025-08-29 14:44:30 +0530

    optmized added for 3_sum

--- Java/3Sum.java
+++ Java/3Sum.java
@@ -1,7 +1,8 @@
+import java.util.*;
 class Solution {
     public List<List<Integer>> threeSum(int[] arr) {
         Set<List<Integer>> set = new HashSet<>();
-        int[] ans = new int[arr.length];
+        
         for(int i = 0; i < arr.length; i++){
             for(int j = i+1; j < arr.length; j++){
                 for(int k = j+1; k < arr.length; k++){
@@ -14,5 +15,71 @@ class Solution {
             }
         }
     return new ArrayList<>(set);    
+    }
+}
+
+
+// #better Solution
+class Solution {
+    public List<List<Integer>> threeSum(int[] arr) {
+        Set<List<Integer>> set = new HashSet<>();
+        
+        for(int i = 0; i < arr.length; i++){
+            Set<Integer> seen = new HashSet<>();
+            for(int j = i+1; j < arr.length; j++){
+
+                int third  = -(arr[i] + arr[j]);
+
+                if(seen.contains(third)){
+
+                List<Integer> temp = Arrays.asList(arr[i],arr[j],third);
+                Collections.sort(temp);
+                set.add(temp);
+
+                }
+                seen.add(arr[j]);
+                
+            }
+        }
+    return new ArrayList<>(set);    
+    }
+}
+
+// optimised 
+class Solution {
+    public List<List<Integer>> threeSum(int[] arr) {
+        List<List<Integer>> triplet = new ArrayList<>();
+        int n = arr.length;
+        Arrays.sort(arr);
+
+        for(int i = 0; i < n; i++){
+            if(i > 0 && arr[i] == arr[i-1]) continue;
+
+            int j = i + 1;
+            int k = n - 1;
+
+            while(j < k){
+                int sum = arr[i] + arr[j] + arr[k];
+
+                if(sum < 0){
+                    j++;
+                    
+                }
+                else if(sum > 0){
+                    k--;
+                }
+                else{
+                    triplet.add(Arrays.asList(arr[i],arr[j],arr[k]));
+                    j++;
+                    k--;
+                    while(j < k && arr[j] == arr[j-1]) j++;
+                    while(j < k && arr[k] == arr[k+1]) k--;
+
+                }
+
+            }
+
+        }    
+    return triplet;    
     }
 }
