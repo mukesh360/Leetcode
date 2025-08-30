a4bd03332b676479464032c6567ffac86f20acb4
mukesh360 <mugimugesh023@gmail.com>
2025-08-30 05:37:27 +0530

    4_sum Added

---
+++ Java/4_sum.java
@@ -0,0 +1,39 @@
+
+import java.util.*;
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        int n = nums.length;
+
+        for(int i = 0; i < n-3; i++){
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+            for(int j = i + 1; j < n-2; j++){
+                if(j > i + 1 && nums[j] == nums[j - 1]) continue;
+                int k = j + 1;
+                int m = n - 1;
+
+                while(k < m){
+                    long sum = (long)nums[i] + nums[j] + nums[k] + nums[m];
+
+                    if(sum < target){
+                        k++;
+                    }
+                    else if(sum > target){
+                        m--;
+                    }
+                    else if(sum == target){
+                        result.add(Arrays.asList(nums[i] , nums[j] , nums[k] , nums[m]));
+
+                        k++;
+                        m--;
+
+                        while(k < m && nums[k] == nums[k - 1]) k++;
+                        while(k < m && nums[m] == nums[m + 1]) m--;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
