62a3ac2bade264c5f10017b29d0d5150bac86bc4
mukesh360 <mugimugesh023@gmail.com>
2025-08-31 07:23:30 +0530

    maths problem added and binary search added

---
+++ Math problem/Reverse Integer.java
@@ -0,0 +1,34 @@
+// only positive 
+class Solution {
+    public int reverse(int x) {
+        int reverse = 0;
+        while (x > 0) {
+            int lastDigit = x % 10;
+            reverse = (reverse * 10) + lastDigit;
+            x = x / 10;
+        }
+        return reverse;
+    }
+}
+
+/// both +ve and -Ve
+class Solution {
+    public int reverse(int x) {
+        int reverse = 0;
+
+        while (x != 0) {
+            int lastDigit = x % 10;
+            x = x / 10;
+            if (reverse > Integer.MAX_VALUE / 10 || (reverse == Integer.MIN_VALUE / 10 && lastDigit > 7)) {
+                return 0;
+            }
+            if (reverse < Integer.MIN_VALUE / 10 || (reverse == Integer.MIN_VALUE / 10 && lastDigit < -8)) {
+                return 0;
+            }
+
+            reverse = (reverse * 10) + lastDigit;
+
+        }
+        return reverse;
+    }
+}
