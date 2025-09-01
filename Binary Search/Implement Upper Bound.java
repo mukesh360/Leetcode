04324576ae86a893dce7b083f1386540695c4cd5
mukesh360 <mugimugesh023@gmail.com>
2025-09-02 05:18:28 +0530

    upper bound implemenattion added

---
+++ Binary Search/Implement Upper Bound.java
@@ -0,0 +1,24 @@
+class Solution {
+    int upperBound(int[] arr, int target) {
+        // code here
+        int lb = 0;
+        int n = arr.length;
+        int ub = n - 1;
+        
+        
+        int ans = n;
+        
+        while(lb <= ub){
+            int mid = (lb + ub)/2;
+            
+            if(arr[mid] > target){
+                ans = mid;
+                ub = mid - 1;
+            }
+            else{
+                lb = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
