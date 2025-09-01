551132d0e1e27901bd88a0930104495699c702cf
mukesh360 <mugimugesh023@gmail.com>
2025-09-02 05:27:08 +0530

    using lb to implement search_insert_position  added

---
+++ Binary Search/search_insert_position.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int searchInsert(int[] arr, int target) {
+        int lb = 0;
+        int n = arr.length;
+        int ub = n - 1;
+        
+        
+        int ans = n;
+        
+        while(lb <= ub){
+            int mid = (lb + ub)/2;
+            
+            if(arr[mid] >= target){
+                ans = mid;
+                ub = mid - 1;
+            }
+            else{
+                lb = mid + 1;
+            }
+        }
+        return ans;
+        
+    }
+}
