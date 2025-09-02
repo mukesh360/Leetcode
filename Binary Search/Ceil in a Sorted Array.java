a2fa05e5221699d65858252e78500808389b5218
mukesh360 <mugimugesh023@gmail.com>
2025-09-02 05:55:03 +0530

    ceil  added

---
+++ Binary Search/Ceil in a Sorted Array.java
@@ -0,0 +1,24 @@
+// User function Template for Java
+class Solution {
+    public int findCeil(int[] arr, int x) {
+        int n = arr.length;
+        int low = 0;
+        
+        int high = n - 1;
+        int ans = -1;
+        
+        while(low <= high){
+            
+            int mid = (low + high) / 2;
+            
+            if(arr[mid] >= x){
+                ans = mid;
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
