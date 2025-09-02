bf739e3bd0f64c58564c27208fa64c06b5648cd2
mukesh360 <mugimugesh023@gmail.com>
2025-09-02 06:21:46 +0530

    first and last occurance   added

--- Binary Search/First and Last Position.java
+++ Binary Search/First and Last Position.java
@@ -17,3 +17,66 @@ class Solution {
         
     }
 }
+//
+class Solution {
+    private int lowerBound(int[] arr, int target) {
+        int lb = 0;
+        int n = arr.length;
+        int ub = n - 1;
+        
+        
+        int ans = n;
+        
+        while(lb <= ub){
+            int mid = (lb + ub)/2;
+            
+            if(arr[mid] >= target){
+                ans = mid;
+                ub = mid - 1;
+            }
+            else{
+                lb = mid + 1;
+            }
+        }
+        return ans;
+        
+    }
+
+    private int upperBound(int[] arr, int target) {
+        // code here
+        int lb = 0;
+        int n = arr.length;
+        int ub = n - 1;
+        
+        
+        int ans = n;
+        
+        while(lb <= ub){
+            int mid = (lb + ub)/2;
+            
+            if(arr[mid] > target){
+                ans = mid;
+                ub = mid - 1;
+            }
+            else{
+                lb = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    public int[] searchRange(int[] nums, int target) {
+
+        
+        int lb = lowerBound(nums, target);
+
+        if(lb == nums.length || nums[lb] != target){
+            return new int[] {-1 , -1}; 
+        }
+
+        int up = upperBound(nums, target);
+
+        return new int[] {lb , up - 1};
+        
+    }
+}
