9ad67284a8a9158a1785d49154f528144e5d12c6
mukesh360 <mugimugesh023@gmail.com>
2025-09-04 10:11:59 +0530

    singleElementInSortedArray.java optimized  added

--- Binary Search/singleElementInSortedArray.java
+++ Binary Search/singleElementInSortedArray.java
@@ -1,3 +1,4 @@
+// brute force 
 class Solution {
     public int singleNonDuplicate(int[] nums) {
         
@@ -15,6 +16,34 @@ class Solution {
             else{
                 if(nums[i] != nums[i - 1] && nums[i] != nums[i + 1]) return nums[i];
             }
+        }
+        return  0;
+    }
+}
+
+
+// optimized 
+class Solution {
+    public int singleNonDuplicate(int[] arr) {
+        
+        int n = arr.length;
+
+        if(n == 1) return arr[0];
+        if(arr[0] != arr[1]) return arr[0];
+        if(arr[n - 1] != arr[n - 2]) return arr[n - 1];
+
+        int low = 1;
+        int high = n - 2;
+
+        while(low <= high){
+            int mid = (low + high) / 2;
+
+            if(arr[mid] != arr[mid - 1] && arr[mid] != arr[mid +1]){
+                return arr[mid];
+            }
+            else if((mid % 2 == 1 && arr[mid] == arr[mid - 1]) || (mid % 2 == 0 && arr[mid] == arr[mid + 1])) low = mid  + 1;
+            
+            else high = mid - 1;
         }
         return  0;
     }
