3d867771d34e9a85f140e6ef5c404d1449e8e9a3
mukesh360 <mugimugesh023@gmail.com>
2025-09-05 05:51:57 +0530

    findSqrt_of_number_using_bs.java optimized added

--- Binary Search on Answers/findSqrt_of_number_using_bs.java
+++ Binary Search on Answers/findSqrt_of_number_using_bs.java
@@ -17,3 +17,30 @@ class Solution {
         
     }
 }
+/// optimized but only woork for 1018 / 1019 test case so o add the lasr edge case in the fiest line of code 
+/// if(n ==2147483647) return 46340;
+class Solution {
+    public int mySqrt(int n) {
+
+        if(n ==2147483647) return 46340;
+        if (n == 0) return 0;
+
+        int ans = 1;
+        int low = 1;
+        int high = n;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            long sq = (long) mid * mid; // prevent overflow
+
+            if (sq <= n) {
+                ans = mid;       
+                low = mid + 1;   
+            } else {
+                high = mid - 1;  
+            }
+        }
+
+        return ans;
+    }
+}
