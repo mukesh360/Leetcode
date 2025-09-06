a72eb97f543d87caf6ccc419649736e1a4aa1b48
mukesh360 <mugimugesh023@gmail.com>
2025-09-06 13:01:44 +0530

    smallest divisor  optimized added

---
+++ Binary Search on Answers/Find the Smallest Divisor Given a Threshold.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int max(int[] arr){
+        int max_num = 0;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] > max_num){
+                max_num = arr[i];
+            }
+        }
+        return max_num;
+
+    }
+    public int sumOfD(int[] arr, int mid){
+        
+        int sum = 0;
+        for(int i = 0; i < arr.length; i++){
+            sum += (int)Math.ceil((double)arr[i] / mid);
+        }
+        return sum;
+
+    }
+    public int smallestDivisor(int[] nums, int threshold) {
+
+        int low = 1;
+        int high = max(nums);
+        int ans = -1;
+
+        while(low <= high){
+            int mid = (low + high) / 2;
+
+            if(sumOfD(nums,mid) <= threshold){
+                ans = mid;
+                high = mid - 1;
+            }
+            else low = mid + 1;
+        }
+
+
+
+        return ans;
+        
+    }
+}
