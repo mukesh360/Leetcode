3a03906e18aba12912738d2784cfa64c5148f7f4
mukesh360 <mugimugesh023@gmail.com>
2025-09-09 05:52:26 +0530

    Aggressive Cows  optimized added

--- Binary Search on Answers/Aggressive Cows.java
+++ Binary Search on Answers/Aggressive Cows.java
@@ -26,4 +26,35 @@ class Solution {
         return max - min; 
     }
 }
-// optimal
+// optimal using bs
+class Solution {
+    
+    private boolean canWePlace(int[] arr, int dist, int cows){
+        int last = arr[0];
+        int cntCows = 1;
+        
+        for(int i = 1; i < arr.length; i++){
+            if(arr[i] - last >= dist){
+                cntCows++;
+                last = arr[i];
+            }
+        }
+        return cntCows >= cows;
+    }
+    public int aggressiveCows(int[] stalls, int k) {
+        Arrays.sort(stalls);
+        int low = 1;
+        int high = stalls[stalls.length - 1] - stalls[0]; //max - mim
+        int ans = -1;
+        while(low <= high){
+            int mid = (low + high) / 2;
+            
+            if(canWePlace(stalls , mid , k) == true){
+                ans = mid;
+                low = mid + 1;
+            }
+            else high = mid - 1;
+        }
+        return ans;
+    }
+}
