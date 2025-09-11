f32a77ef299ea2daaf00f3e666e4df301bc642fe
mukesh360 <mugimugesh023@gmail.com>
2025-09-11 07:06:33 +0530

    Median of Two Sorted Arrays optimal  added

--- Binary Search on Answers/Median of Two Sorted Arrays.java
+++ Binary Search on Answers/Median of Two Sorted Arrays.java
@@ -41,6 +41,50 @@ class Solution {
         
     }
 }
+// above case
+class Solution {
+    private int[] mergeArray(int[] nums1, int[] nums2) {
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        int[] merged = new int[n1 + n2];
+        
+        int i = 0, j = 0, k = 0;
+        
+        // Merge like in merge sort
+        while (i < n1 && j < n2) {
+            if (nums1[i] < nums2[j]) {
+                merged[k++] = nums1[i++];
+            } else {
+                merged[k++] = nums2[j++];
+            }
+        }
+        
+        // Copy remaining elements
+        while (i < n1) {
+            merged[k++] = nums1[i++];
+        }
+        while (j < n2) {
+            merged[k++] = nums2[j++];
+        }
+        
+        return merged;
+    }
+
+    private double findMedian(int[] nums) {
+        int n = nums.length;
+        if (n % 2 == 1) {
+            return nums[n / 2];
+        } else {
+            return (nums[n / 2 - 1] + nums[n / 2]) / 2.0;
+        }
+    }
+
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int[] merged = mergeArray(nums1, nums2);
+        return findMedian(merged);
+    }
+}
+
 /// optimal
 class Solution {
     public double findMedianSortedArrays(int[] a, int[] b) {
