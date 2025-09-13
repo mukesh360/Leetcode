41ea1d2f4894f963cb88f5b897727077732cc922
mukesh360 <mugimugesh023@gmail.com>
2025-09-13 06:04:51 +0530

    two sum.java optimal added

--- Arrays/Java/Two Sum.java
+++ Arrays/Java/Two Sum.java
@@ -1,12 +1,15 @@
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        
-        for(int i = 0; i < nums.length; i++){
-            for(int j = i + 1; j < nums.length; j++){
-                if(nums[i] + nums[j] == target){
-                    return   new int[]{i , j};
-                }
+    public int[] twoSum(int[] arr, int target) {
+
+        Map<Integer,Integer> set = new HashMap<>();
+
+        for(int i = 0; i < arr.length; i++){
+            int addup = target - arr[i];
+            if(set.containsKey(addup)){
+                return new int[] {i , set.get(addup)};
             }
+            set.put(arr[i] , i);
+
         }
         return new int[]{};
         
