853940ac29f5adcf5df95ffafed996ccb0819bf9
mukesh360 <mugimugesh023@gmail.com>
2025-09-19 06:04:29 +0530

    Largest subarray with 0 sum.java added

---
+++ Arrays/Java/Largest subarray with 0 sum.java
@@ -0,0 +1,27 @@
+class Solution {
+    int maxLength(int arr[]) {
+        // code here
+        HashMap<Integer , Integer> map = new HashMap<>();
+        
+        int sum = 0;
+        int max = 0;
+        
+        int n = arr.length;
+        
+        for(int i = 0; i < n; i++){
+            sum += arr[i];
+            
+            if(sum == 0){
+                max = i + 1;
+            }
+            else if(map.containsKey(sum)){
+                max = Math.max(max ,i - map.get(sum));
+            }
+            else{
+                map.put(sum , i);
+            }
+        
+        }
+        return max;
+    }
+}
