42ba60896450e11686dad2bfaed873fdc6cee9d7
mukesh360 <mugimugesh023@gmail.com>
2025-09-22 06:41:32 +0530

    plusOne.java  added

--- Arrays/Largest Element - Copy.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def largestElement(self, nums):
-        max_number = nums[0] 
-        for number in nums:
-            if number > max_number:
-                max_number = number
-        return max_number
-        
