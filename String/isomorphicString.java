992c405b1cbeca95873a3e933ad9bf1ae5102212
mukesh360 <mugimugesh023@gmail.com>
2025-12-12 12:27:29 +0530

    string added

---
+++ String/isomorphicString.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        
+        int n = s.length();
+
+        int[] hash = new int[256];
+        Arrays.fill(hash , 0);
+
+        boolean[] isMapped = new boolean[256];
+        Arrays.fill(isMapped , false);
+
+        for(int i = 0; i < n; i++){
+
+            if(hash[s.charAt(i)] == 0 && isMapped[t.charAt(i)] == false){
+
+                hash[s.charAt(i)] = t.charAt(i);
+                isMapped[t.charAt(i)] = true;
+            }
+        }
+
+        for(int i = 0; i < n; i++){
+            if((char) (hash[s.charAt(i)]) != t.charAt(i)){
+                return false;
+            }
+        }
+
+
+    return true;
+    }
+}
---
+++ String/reverseWord.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String reverseWords(String s) {
+
+        String[] word = s.trim().split("\\s+");
+        StringBuilder ans = new StringBuilder();
+
+        for(int i = word.length - 1; i >= 0; i--){
+
+            ans.append(word[i] + " ");
+        }
+     return ans.toString().trim();
+    }
+}
