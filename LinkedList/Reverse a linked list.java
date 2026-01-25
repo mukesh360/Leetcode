b5acfbb6bbd935e0978352daadc317c8af9aa75f
mukesh360 <mugimugesh023@gamil.com>
2026-01-25 16:19:02 +0530

    revrese linked list  added

--- LinkedList/Reverse a linked list.java
+++ LinkedList/Reverse a linked list.java
@@ -28,3 +28,20 @@ class Solution {
         return head;
     }
 }
+
+// more cclean way
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+
+        while (current != null) {
+            ListNode next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        head = prev;
+        return prev;
+    }
+}
