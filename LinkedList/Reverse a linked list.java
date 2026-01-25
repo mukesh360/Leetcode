0d29e20f0d88ca59a2482424d729799d99235300
mukesh360 <mugimugesh023@gamil.com>
2026-01-25 16:09:32 +0530

    revrese linked list  added

---
+++ LinkedList/Reverse a linked list.java
@@ -0,0 +1,30 @@
+/*
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+*/
+
+class Solution {
+    Node reverseList(Node head) {
+        // code here
+        Node prev = null;
+        Node current = head;
+        Node next = head.next;
+        
+        while(current != null){
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+            
+        }
+        head = prev;
+        return head;
+    }
+}
