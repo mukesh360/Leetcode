8731b50f2c5b3d2867d6bcabf17553c9ac0b9e91
mukesh360 <mugimugesh023@gmail.com>
2026-03-03 21:09:32 +0530

    Min Stack added

--- stcak and Queues/Min Stack.java
+++ stcak and Queues/Min Stack.java
@@ -1,3 +1,50 @@
-public class Min Stack {
-    
+import java.util.*;
+
+class MinStack {
+
+    Stack<Long> st;
+    long min;
+
+    public MinStack() {
+        st = new Stack<>();
+    }
+
+    public void push(int val) {
+
+        if (st.isEmpty()) {
+            st.push((long)val);
+            min = val;
+        } 
+        else if (val >= min) {
+            st.push((long)val);
+        } 
+        else {
+            // encode value
+            st.push(2L * val - min);
+            min = val;
+        }
+    }
+
+    public void pop() {
+        if (st.isEmpty()) return;
+
+        long x = st.pop();
+
+        if (x < min) {
+            min = 2 * min - x;
+        }
+    }
+
+    public int top() {
+        long x = st.peek();
+
+        if (x < min)
+            return (int)min;
+
+        return (int)x;
+    }
+
+    public int getMin() {
+        return (int)min;
+    }
 }
