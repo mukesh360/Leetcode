16e5ffb91476ff83539f31b1454380ec07867e2c
mukesh360 <mugimugesh023@gmail.com>
2026-03-30 21:26:39 +0530

    online stock span  added

---
+++ stcak and Queues/Online Stock Span.java
@@ -0,0 +1,22 @@
+class StockSpanner {
+    List<Integer> arr;
+
+    public StockSpanner() {
+        arr = new ArrayList<>();
+    }
+    
+    public int next(int price) {
+        arr.add(price);
+        int span = 1;
+
+        for (int i = arr.size() - 2; i >= 0; i--) {
+            if (arr.get(i) <= price) {
+                span++;
+            } else {
+                break;
+            }
+        }
+
+        return span;
+    }
+}
